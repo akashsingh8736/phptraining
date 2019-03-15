62e503d815247aa73844e223590f47574b59be81
Jaswinder Singh <sjeswinder@jstrainer.com>
2019-03-15 23:09:48 +0530

    Updated code for login form

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # phptraining
+
+A sample login form with submit to php program for display use of IF construct and output. 
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Demo</title>
+</head>
+<body>
+	Welcome to PHP training! 
+	<a href='form.php'>LOGIN</a><br>
+</body>
+</html>
